0d110160997ef6143a7de4360100b74f74b1ef46
coliveiracodes <coliveira1881@gmail.com>
2025-09-23 10:55:08 +0100

    Create main.c
    Programa em Linguagem C, retorna Ano Bissexto ou não

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Programa que retorna a informacao que que o ano = bissexto
+Condicoes: 
+- De 400 em 400 anos = bissexto
+- De 4 em 4 anos = bissexto
+- De 100 em 100 anos nao e bissexto*/
+
+
+int main()
+{
+    int ano;
+    printf("Escreva o ano para saber se e bissexto: \n");
+    scanf("%d",&ano);
+    if(ano%400==0 || ano%4==0 && ano%100!=0 ){
+        printf("O ano %d e bissexto!",ano);
+      }else{
+         printf("O ano %d nao e bissexto!",ano);
+    }
+
+    return 0;
+}
