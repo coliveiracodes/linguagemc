12e6659be658182bab3c2ae0bd8f154d1d611015
coliveiracodes <coliveira1881@gmail.com>
2025-09-23 11:43:31 +0100

    Retornar a informacao de que o ano = bissexto

--- anobissexto.c
+++ anobissexto.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*Programa que retorna a informacao que que o ano = bissexto
+
+/*
+Programa que retorna a informacao que que o ano = bissexto
 Condicoes: 
 - De 400 em 400 anos = bissexto
 - De 4 em 4 anos = bissexto
-- De 100 em 100 anos nao e bissexto*/
+- De 100 em 100 anos nao e bissexto
+*/
 
 
 int main()
